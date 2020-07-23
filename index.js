800e145fda990c4c4ffdee73bac77d0e4bcfef4e
chandu03 <chandragupta.singh4@gmail.com>
2020-07-23 22:01:13 +0530

    Node-Callbacks-Error-Handling

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var rect = require('./rectangle')
 
 function solveRect(l,b){
-    console.log("Solveing for rect for l= "+ l +" b= "+b)
-    if(l<=0 || b<=0){
-        console.log("invalid values")
-    }
-    else{
-        console.log("perimeter is "+ rect.perimeter(l,b))
-        console.log("area is "+ rect.area(l,b))
-    }
+    console.log("Solving for rect for l= "+ l +" b= "+b)
+    rect(l,b, (err, rectangle) => {
+        if(err){
+            console.log("error ", err.message)
+        }
+        else{
+            console.log("the area is: "+ rectangle.area())
+            console.log("the perimeter is: "+ rectangle.perimeter())
+        }
+    })
+    console.log("this is after rect()")
 }
 
 solveRect(2,4)
